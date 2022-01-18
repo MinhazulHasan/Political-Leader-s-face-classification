769b16f1f74f367ce55d960cc34e70f0c1544810
Minhazul Hasan Sohan <>
2022-01-19 01:07:26 +0600

    first commit

---
+++ model/Jupyter Notebook.bat
@@ -0,0 +1,5 @@
+:: Change Directory in cmd
+
+cd /d C:\Users\ASUS\OneDrive\Desktop\Political Leader's face classification\model
+
+start "Sohan Script" cmd.exe /K "C:\Users\ASUS\anaconda3\Scripts\activate.bat &jupyter notebook"
